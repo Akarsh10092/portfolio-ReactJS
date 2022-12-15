f681779d4b3bded8b241b115939fe35b492de70b
Akarsh Agarwal <akarshagarwal9863@gmail.com>
2022-12-15 20:40:48 +0530

    ch5

--- src/components/Info1.js
+++ src/components/Info1.js
@@ -4,19 +4,21 @@ import pro1 from "../images/pro1.jpg"
 import js1 from "../images/js1.png"
 import lights from "../images/lights.jpg"
 const Info1 = () => {
+  const item = { lightsout: "https://lightsout-psi.vercel.app/",
+}
   return (
     <div className='info1'>
     <div className='project1'>
     <img classname="img1" src={pro1} alt="true"></img>
-        <h1>EcomApp</h1>
+       <h1><btn className="btn">EcomApp</btn> </h1>
         <p>The webapp is fully functioning website with both working frontend/backend
         <p>Skills used: MongoDB/Express/React/Nodejs</p></p>
         <img classname="img2" src={js1} alt="true"></img>
-        <h1>GoalSetter App</h1>
+        <h1><btn className="btn">GoalSetter App</btn></h1>
         <p><p>The webapp is fully functioning website with both working frontend/backend</p>
             Skills used: MongoDB/Express/React/Node.JS</p>
         <img classname="img3" src={lights} alt="true"></img>
-        <h1>Lightsout</h1>
+        <a href={item.lightsout} target="_blank" rel="noopener noreferrer"><h1><btn className="btn">Lightsout</btn></h1></a>
         <p>This game was developed using the implementation of React class component and basic VanillaJS</p>  
     </div>
 </div>
