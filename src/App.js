741d8ee8781dfbd6788060a0738be73fa18695e0
Akarsh Agarwal <akarshagarwal9863@gmail.com>
2022-12-15 12:07:38 +0530

    ch1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
-
+import './index.css';
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Home from './routes/Home';
+import Project from './routes/Project';
+import {Routes,Route} from "react-router-dom"
+import Information from './routes/Information';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         Hello world
-        </p>
-      </header>
-    </div>
+    <>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+   <Route path='/project' element={<Project/>}/>
+    <Route path='/contact' element={<Contact/>}/>
+    <Route path='/information' element={<Information/>}/>
+    </Routes>
+  </>
   );
 }
 
